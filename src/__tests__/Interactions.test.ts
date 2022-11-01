5c8a22aa861f918b78b81d7982a9772b5ee2d66a
Callum Gibson <vfmx78@durham.ac.uk>
2022-11-01 12:17:13 +0000

    1.1.0

--- src/__tests__/Interactions.test.ts
+++ src/__tests__/Interactions.test.ts
@@ -1,43 +1,43 @@
 import { Greeter, Carter } from '../index';
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 test('Greeter Test', () => {
   expect(Greeter('LazyLyrics')).toBe('Hello LazyLyrics!');
 });
 
 test('Say Test 1', async () => {
-  const apiKey = process.env.CARTER_API_KEY
+  const apiKey = process.env.CARTER_API_KEY;
   let carter: Carter;
   if (apiKey) {
-    carter = new Carter(apiKey)
-    let response = await carter.say("Hello")
-    expect(response.ok).toBe(true)
+    carter = new Carter(apiKey);
+    let response = await carter.say('Hello');
+    expect(response.ok).toBe(true);
   } else {
-    throw Error("No API key found.")
+    throw Error('No API key found.');
   }
-})
+});
 
 test('Say Test 2', async () => {
-  const apiKey = process.env.CARTER_API_KEY
+  const apiKey = process.env.CARTER_API_KEY;
   let carter: Carter;
   if (apiKey) {
-    carter = new Carter(apiKey)
-    let response = await carter.say("Hello", "hasfasf", "level-one")
-    expect(response.ok).toBe(true)
+    carter = new Carter(apiKey);
+    let response = await carter.say('Hello', 'hasfasf', 'level-one');
+    expect(response.ok).toBe(true);
   } else {
-    throw Error("No API key found.")
+    throw Error('No API key found.');
   }
-})
+});
 
 test('Say Test 3', async () => {
-  const apiKey = process.env.CARTER_API_KEY
+  const apiKey = process.env.CARTER_API_KEY;
   let carter: Carter;
   if (apiKey) {
-    carter = new Carter(apiKey)
-    let response = await carter.say("Hello", undefined, "level-one")
-    expect(response.ok).toBe(true)
+    carter = new Carter(apiKey);
+    let response = await carter.say('Hello', undefined, 'level-one');
+    expect(response.ok).toBe(true);
   } else {
-    throw Error("No API key found.")
+    throw Error('No API key found.');
   }
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 // import fetch from 'node-fetch'
-import type { CarterPayload, CarterResponse } from './types'
+import type { CarterPayload, CarterResponse } from './types';
 import { v1 as uuidv1 } from 'uuid';
 
 export const Greeter = (name: string) => `Hello ${name}!`;
 
-
-
 export class Carter {
-  apiKey: string
+  apiKey: string;
 
   constructor(apiKey: string) {
     this.apiKey = apiKey;
@@ -15,29 +13,28 @@ export class Carter {
 
   async say(query: string, uuid?: string, scene?: string): Promise<CarterResponse> {
     const payload: CarterPayload = {
-      "api_key": this.apiKey,
-      "query": query
-    }
+      api_key: this.apiKey,
+      query: query,
+    };
     if (scene) {
-      payload.scene = scene
+      payload.scene = scene;
     }
     if (uuid) {
-      payload.uuid = uuid
+      payload.uuid = uuid;
     } else {
-      payload.uuid = uuidv1()
+      payload.uuid = uuidv1();
     }
-    const response = await fetch("https://api.carterapi.com/v0/chat", {
-      method: "POST",
+    const response = await fetch('https://api.carterapi.com/v0/chat', {
+      method: 'POST',
       body: JSON.stringify(payload),
-      headers: { 'Content-Type': 'application/json' }
-    })
+      headers: { 'Content-Type': 'application/json' },
+    });
     return {
       data: await response.json(),
       ok: response.ok,
       statusCode: response.status,
       statusMessage: response.statusText,
-      payload
-    }
+      payload,
+    };
   }
-
 }
